f070a7d776655789a995a4411327928583515bce
khime_dev <145382798+XtreamVox@users.noreply.github.com>
2026-04-21 21:23:25 +0200

    Corrección del controller

--- src/controllers/client.controller.js
+++ src/controllers/client.controller.js
@@ -2,7 +2,7 @@ import Client from "../models/client.models.js";
 import { AppError } from "../utils/AppError.js";
 import User from "../models/user.models.js";
 
-export async function createClient(req, res, error) {
+export async function createClient(req, res, next) {
   try {
     const { name, cif, email, phone, address } = req.body;
 
@@ -13,19 +13,19 @@ export async function createClient(req, res, error) {
     const client = await Client.create({
       user: req.user._id,
       company: req.user.company,
-      name,
-      cif,
-      email,
-      phone,
-      address,
+      name: name,
+      cif: cif,
+      email: email,
+      phone: phone,
+      address: phone,
     });
     res.status(201).json(client);
-  } catch {
+  } catch (error) {
     next(error);
   }
 }
 
-export async function updateClient(req, res, error) {
+export async function updateClient(req, res, next) {
   try {
     const { id } = req.params;
 
@@ -35,26 +35,27 @@ export async function updateClient(req, res, error) {
     const client = await Client.findByIdAndUpdate(id, obj);
 
     res.status(200).json(obj);
-  } catch {
+  } catch (error) {
     next(error);
   }
 }
 
 // ASK listar con populate?
-export async function getAllClients(req, res, error) {
+export async function getAllClients(req, res, next) {
   try {
-    const { limit, sort } = req.query;
+    // TODO gestionar querys y establecer defaults con zod
+    const { limit, sort, page, filter } = req.query;
     // TODO hacer esto en zod: limit ? minimum(limit, 100) : 1;
-    let clients;
     const skip = (page - 1) * limit;
-    clients = await Client.find()
+    const clients = await Client.find(filter)
       .populate("Company", "User")
+      .skip(skip)
       .limit(limit)
       .sort({ createdAt: -1 });
 
     const total = Client.countDocuments();
     res.json({
-      data: users,
+      data: clients,
       pagination: {
         total,
         page,
@@ -64,24 +65,24 @@ export async function getAllClients(req, res, error) {
         hasPrevPage: page > 1,
       },
     });
-  } catch {
+  } catch (error) {
     next(error);
   }
 }
 
-export async function getClient(req, res, error) {
+export async function getClient(req, res, next) {
   try {
     const { id } = req.params;
 
     const client = await Client.findById(id);
 
     res.status(200).json(client);
-  } catch {
+  } catch (error) {
     next(error);
   }
 }
 
-export async function deleteUser(req, res, next) {
+export async function deleteClient(req, res, next) {
   try {
     // TODO hacer que llegue como bool en zod
     const { soft } = req.query;
@@ -96,7 +97,7 @@ export async function deleteUser(req, res, next) {
     }
 
     res.status(200).json(client);
-  } catch {
+  } catch (error) {
     next(error);
   }
 }
@@ -105,19 +106,19 @@ export async function listArchivedClients(req, res, next) {
   try {
     const clients = Client.findDeleted();
     res.status(200).json(clients);
-  } catch {
+  } catch (error) {
     next(error);
   }
 }
 
 export async function restoreArchivedClientById(req, res, next) {
   try {
-    const {id} = req.params;
+    const { id } = req.params;
 
     // ASK gestionar distintos errores de existencia ??
     const clients = Client.restoreById();
     res.status(200).json(clients);
-  } catch {
+  } catch (error){
     next(error);
   }
 }
