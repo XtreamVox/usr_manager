4483e259c70d16ba81726d904ac5cbbf34844bb4
khime_dev <145382798+XtreamVox@users.noreply.github.com>
2026-04-25 14:33:11 +0200

    rutas de los albaranes

---
+++ src/routes/deliverynote.routes.js
@@ -0,0 +1,25 @@
+import { Router } from "express";
+import authMiddleware from "../middleware/auth.midddleware.js";
+import checkRol from "../middleware/role.middleware.js";
+import { validate, validateFile } from "../middleware/validate.middleware.js";
+import { checkForCompany } from "../middleware/client_checks.middleware.js";
+
+import {
+  createDeliveryNote,
+  deleteDeliveryNote,
+  getAllDeliveryNotes,
+  getDeliveryNote,
+  getPdfFromDeliveryNote,
+  signPdf,
+} from "../controllers/deliveryNote.controller.js";
+
+import { createDeliveryNoteScheme, DeliveryNotePaginationAndFilterScheme, validateDeliveryNoteIdScheme , validateDeleteIdScheme, sortDeleteDeliveryNoteScheme} from "../squemes/deliveryNote.squeme.js";
+
+const router = Router();
+
+router.post("/", authMiddleware, checkForCompany, validate({ body: createDeliveryNoteScheme}), createDeliveryNote)
+router.get("/", authMiddleware, checkForCompany, validate({query : DeliveryNotePaginationAndFilterScheme}) ,getAllDeliveryNotes)
+router.get("/:id", authMiddleware, checkForCompany, validate({params : validateDeliveryNoteIdScheme}), getDeliveryNote)
+router.get("/pdf/:id", authMiddleware, checkForCompany, validate({params : validateDeliveryNoteIdScheme}), getPdfFromDeliveryNote)
+router.patch("/:id/sign", authMiddleware, checkForCompany, validate({params : validateDeliveryNoteIdScheme}), signPdf)
+router.delete("/:id", authMiddleware, checkForCompany, checkRol("admin"), validate({params: validateDeleteIdScheme}), validate({query: sortDeleteDeliveryNoteScheme}), deleteDeliveryNote)
