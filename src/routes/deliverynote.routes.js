843118d5950813556f8be7152c3a49331face5c0
khime_dev <145382798+XtreamVox@users.noreply.github.com>
2026-04-29 23:51:54 +0200

    Corrección del orden de las rutas

--- src/routes/deliverynote.routes.js
+++ src/routes/deliverynote.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
+import uploadMiddleware from "../utils/handleStorage.utils.js";
 import authMiddleware from "../middleware/auth.midddleware.js";
 import checkRol from "../middleware/role.middleware.js";
-import { validate, validateFile } from "../middleware/validate.middleware.js";
+import { validate } from "../middleware/validate.middleware.js";
 import { checkForCompany } from "../middleware/client_checks.middleware.js";
 
 import {
@@ -20,15 +21,14 @@ import {
   validateDeleteIdScheme,
   softDeleteDeliveryNoteScheme
   } from "../squemes/deliveryNote.squeme.js";
-import { downloadPdf } from "../utils/handlePDF.js";
 
 const router = Router();
 
 router.post("/", authMiddleware, checkForCompany, validate({ body: createDeliveryNoteScheme}), createDeliveryNote)
 router.get("/", authMiddleware, checkForCompany, validate({query : DeliveryNotePaginationAndFilterScheme}) ,getAllDeliveryNotes)
-router.get("/:id", authMiddleware, checkForCompany, validate({params : validateDeliveryNoteIdScheme}), getDeliveryNote)
 router.get("/pdf/:id", authMiddleware, checkForCompany, validate({params : validateDeliveryNoteIdScheme}), getPdfFromDeliveryNote)
-router.patch("/:id/sign", authMiddleware, checkForCompany, validate({params : validateDeliveryNoteIdScheme}), downloadPdf, signPdf)
+router.patch("/:id/sign", authMiddleware, checkForCompany, validate({params : validateDeliveryNoteIdScheme}), uploadMiddleware.single("signature"), signPdf)
+router.get("/:id", authMiddleware, checkForCompany, validate({params : validateDeliveryNoteIdScheme}), getDeliveryNote)
 router.delete("/:id", authMiddleware, checkForCompany, checkRol("admin"), validate({params: validateDeleteIdScheme}), validate({query: softDeleteDeliveryNoteScheme}), deleteDeliveryNote)
 
 export default router;
